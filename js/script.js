fa7bfc9e5af402c9c95ec556cd4fe0c5544dd97d
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2023-09-06 02:33:52 -0700

    Add bunch of features
    added heading the line, setup the containers which will be later used and also tweaked the Js code to hide any code that will be present when the navbar for mobile shows u[

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function navOpen() {
     let cont = document.querySelector(".cont");
-    let lf = document.querySelector("header");
+    let lf = document.querySelector("#m");
     let computedStyles = window.getComputedStyle(cont);
   
     if (computedStyles.display === "block") {
       cont.style.display = "none";
+      lf.style.display = "block"
     } else if (computedStyles.display === "none") {
       cont.style.display = "block";
+      lf.style.display = "none"
     }
   }
   
